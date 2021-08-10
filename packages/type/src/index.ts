c74a4a2be71ca55893237c2e60aa5a3df40e3e00
koloboid@gmail.com <koloboid@gmail.com>
2021-08-10 18:51:46 +0300

    Path support filter param, Path exported from @uk/type

--- packages/type/src/index.ts
+++ packages/type/src/index.ts
@@ -1,6 +1,7 @@
-export * from './type';
-export * from './shape';
 export * from './field';
+export * from './path';
+export * from './shape';
+export * from './type';
 import { type } from './type';
 
 export default type;
--- packages/type/src/path.ts
+++ packages/type/src/path.ts
@@ -1,22 +1,23 @@
 import ObjectID from 'bson-objectid';
+import { O } from 'ts-toolbelt';
 import { type } from './type';
 
 export namespace Path {
-    export type Keys<T, E = never> = T extends E
+    export type Keys<T, F = any, E = never> = T extends E
         ? never
         : T extends readonly (infer I)[]
         ? I extends Date | ObjectID | Promise<any>
             ? never
             : I extends object
-            ? Extract<keyof I, string> | SubKeys<I, Extract<keyof I, string>, E | I>
+            ? Extract<O.SelectKeys<T, F>, string> | SubKeys<I, Extract<keyof I, string>, E | I, F>
             : never
         : T extends Date | ObjectID | Promise<any> | Uint8Array | ArrayBuffer
         ? never
         : T extends object
-        ? Extract<keyof T, string> | SubKeys<T, Extract<keyof T, string>, E | T>
+        ? Extract<O.SelectKeys<T, F>, string> | SubKeys<T, Extract<keyof T, string>, E | T, F>
         : never;
 
-    type SubKeys<T, K extends string, E> = K extends keyof T ? `${K}.${Keys<T[K], E>}` : never;
+    type SubKeys<T, K extends string, E, F> = K extends keyof T ? `${K}.${Keys<T[K], F, E>}` : never;
 
     export type Prop<T, Path extends string, V = type.Definable<T>> = Path extends keyof V
         ? V[Path]
